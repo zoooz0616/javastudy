b49b89593662691f013f559beb8cc5e7bd3a72e1
KOSA <zoooz0616@gmail.com>
2023-04-11 09:49:25 +0900

    today first commit

---
+++ thisjava/src/ch06/sec11/exam01/KoreanExample.java
@@ -0,0 +1,16 @@
+package ch06.sec11.exam01;
+
+public class KoreanExample {
+
+	public static void main(String[] args) {
+		Korean k1 = new Korean("123456-1234567", "김자바");
+		
+		System.out.println(k1.nation);
+		System.out.println(k1.ssn);
+		System.out.println(k1.name);
+		
+		
+		k1.name="김자바자바";
+	}
+
+}

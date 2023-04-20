0a5e9b04ecee68e62bc789326db8e9417d451da5
KOSA <zoooz0616@gmail.com>
2023-04-20 16:18:52 +0900

    library code commit

---
+++ thisjava/src/ch12/sec06/WrapperExample.java
@@ -0,0 +1,16 @@
+package ch12.sec06;
+
+import java.util.Date;
+
+
+public class WrapperExample {
+
+	public static void main(String[] args) {
+		Object[] objArr= new Object[5];
+		objArr[0] = "Hello";
+		objArr[1] = new Date();
+//		objArr[2] = 10; //주소 10을 넣어라.
+		objArr[2] = new Integer(10);
+	}
+
+}

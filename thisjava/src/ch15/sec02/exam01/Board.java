9a915831fbf0bf5379d1f568b33a8644cf56ea1a
KOSA <zoooz0616@gmail.com>
2023-04-24 16:10:36 +0900

    commmit

--- thisjava/src/ch15/sec02/exam01/Board.java
+++ thisjava/src/ch15/sec02/exam01/Board.java
@@ -1,5 +1,16 @@
 package ch15.sec02.exam01;
 
-public class Board {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@ToString
+public class Board {
+	private String subject;
+	private String content;
+	private String writerString;
 }
